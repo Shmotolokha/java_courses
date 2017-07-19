e6c898fa3b6f0b6ed9723f19d92e8dfb7fb89366
vova <shmotoloha@gmail.com>
2017-07-19 16:06:52 +0300

    create calculate

--- calculate.java
+++ calculate.java
@@ -1,7 +1,21 @@
 public class calculate
 {
-	public static void main (string[] arg)
+	public static void main (String[] arg)
 	{
-		System.out.println("Calculate");
+		System.out.println("Calculate...");
+		//int first = Integer.valueOf(arg[0]);
+		//int second = Integer.valueOf(arg[1]);
+		
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double sum = first+second;
+		System.out.println("sum="+sum);
+		double riznu = first-second;
+		System.out.println("Riznu="+riznu);
+		double mn = first*second;
+		System.out.println("Mnozennja="+mn);
+		double dil = first/second;
+		System.out.println("Dilennja="+dil);
+		
 	}
 }
